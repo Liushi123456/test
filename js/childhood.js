adfd6d1517ad1896008ab65fc675930e277d432b
Carlshi <1072859307@qq.com>
2016-07-20 20:57:24 +0800

    add test

---
+++ js/childhood.js
@@ -0,0 +1,72 @@
+$("li:eq(0)").bind("mouseover",function(){
+      $(".info_text").hide();
+      $(".text").hide();
+      $(".text_1").show();
+
+      $("li:eq(0)").bind("mouseout",function(){
+            $(".text_1").hide();
+            $(".info_text").show();
+      })
+
+})
+$("li:eq(1)").bind("mouseover",function(){
+      $(".info_text").hide();
+       $(".text").hide();
+      $(".text_2").show();
+
+      $("li:eq(1)").bind("mouseout",function(){
+            $(".text_2").hide();
+            $(".info_text").show();
+      })
+})
+$("li:eq(2)").bind("mouseover",function(){
+      $(".info_text").hide();
+       $(".text").hide();
+      $(".text_3").show();
+
+      $("li:eq(2)").bind("mouseout",function(){
+            $(".text_3").hide();
+            $(".info_text").show();
+      })
+})
+$("li:eq(3)").bind("mouseover",function(){
+      $(".info_text").hide();
+       $(".text").hide();
+      $(".text_4").show();
+
+      $("li:eq(3)").bind("mouseout",function(){
+            $(".text_4").hide();
+            $(".info_text").show();
+      })
+})
+$("li:eq(4)").bind("mouseover",function(){
+      $(".info_text").hide();
+       $(".text").hide();
+      $(".text_5").show();
+
+      $("li:eq(4)").bind("mouseout",function(){
+            $(".text_5").hide();
+            $(".info_text").show();
+      })
+})
+$("li:eq(5)").bind("mouseover",function(){
+      $(".info_text").hide();
+       $(".text").hide();
+      $(".text_6").show();
+
+      $("li:eq(5)").bind("mouseout",function(){
+            $(".text_6").hide();
+            $(".info_text").show();
+      })
+})
+$("li:eq(6)").bind("mouseover",function(){
+      $(".info_text").hide();
+       $(".text").hide();
+      $(".text_7").show();
+
+      $("li:eq(6)").bind("mouseout",function(){
+            $(".text_7").hide();
+            $(".info_text").show();
+      })
+})
+
